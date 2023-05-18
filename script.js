01d638532b77f80629829ab71bfa2dbf15d222d5
sachingupta890 <sachinvarshney459@gmail.com>
2023-05-18 08:26:44 +0530

    media queries add

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// function redirectToPage() {
-//     window.open("login.html", "_blank");
-// }
-const gallery = document.querySelector('.gallery');
-const images = gallery.querySelectorAll('img');
+// // function redirectToPage() {
+// //     window.open("login.html", "_blank");
+// // }
+// const gallery = document.querySelector('.gallery');
+// const images = gallery.querySelectorAll('img');
 
-// Loop through each image in the gallery
-images.forEach((image) => {
-  // Add an event listener for the "click" event
-  image.addEventListener('click', () => {
-    // Get the src attribute of the clicked image
-    const imageUrl = image.getAttribute('src');
-    // Redirect the user to the desired page with the image url as a parameter
-    window.location.href = `random.html?image=${imageUrl}`;
-  });
-});
+// // Loop through each image in the gallery
+// images.forEach((image) => {
+//   // Add an event listener for the "click" event
+//   image.addEventListener('click', () => {
+//     // Get the src attribute of the clicked image
+//     const imageUrl = image.getAttribute('src');
+//     // Redirect the user to the desired page with the image url as a parameter
+//     window.location.href = `random.html?image=${imageUrl}`;
+//   });
+// });
